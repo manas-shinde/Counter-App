a6483947198a8f31b6deab0446d7d52ea4ae2fca
manas-shinde <manas.a.shinde11@gmail.com>
2023-05-27 01:18:14 +0530

    Convert to Stateless Functional Component.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
-class Navbar extends Component {
-  state = {};
-  render() {
-    console.log(this.props);
-    return (
-      <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="manas">
-            Navbar
-            <span className="badge badge-pill badge-secondary">
-              {this.props.totalCounter}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+import React from "react";
 
+const Navbar = (props) => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="manas">
+          Navbar
+          <button className="bg bg-pill bg-warning">
+            {props.totalCounter}
+          </button>
+        </a>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
